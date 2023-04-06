abe14c89054218ebb69e0cb6967ec048ac841b55
hamidpoya <officepoya@gmail.com>
2023-04-05 22:15:55 -0700

    Hamid added some features

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         Book b=new Book();
         b.writer="Pushkin";
         System.out.println(b.writer);
+
+        Hamid h = new Hamid();
+        h.writer="Poya";
+        System.out.println(h.writer);
     }
 }
