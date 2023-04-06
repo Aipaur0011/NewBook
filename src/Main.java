e79493cbd6b13ff2c52878dbaa40030a7a927624
Yuliya Tratsiuk <ytratsiuk@gmail.com>
2023-04-05 23:04:36 -0600

    added third book

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         Book b=new Book();
         b.writer="Pushkin";
         System.out.println(b.writer);
+
+        Yuliya_sBook bo = new Yuliya_sBook();
+        bo.writer="Tolerant";
+        System.out.println(bo.writer);
     }
 }
